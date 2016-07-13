7daa3da3c2ef35cab504c1c11411aa812031852a
Prasanth Maddali <pmaddali@qti.qualcomm.com>
2016-07-13 16:21:35 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,9 @@
+var http =  require('http');
+var server = http.createServer(function(request, response){
+	response.writeHead(200,  {'Content-Type':'text/html'});
+	response.write("safd");
+	response.end('<b>This is for testing nodemon<b/>');
+});
+console.log('Listening on Port 8090');
+
+server.listen(8090);
